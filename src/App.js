6146a8330d2a176fca113ed36c98eb29542fdd99
ansoferreiro <limaf.wanderson@gmail.com>
2024-09-25 19:27:43 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [file, setFile] = useState(null);
+    const [totalValue, setTotalValue] = useState(0);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('file', file);
+        const response = await axios.post('http://127.0.0.1:5000/upload', formData);
+        setTotalValue(response.data.total_value);
+    };
+
+    return (
+        <div>
+            <h1>Análise de Nota Fiscal</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="file" onChange={handleFileChange} />
+                <button type="submit">Enviar</button>
+            </form>
+            <h2>Total: {totalValue}</h2>
+        </div>
+    );
 }
 
 export default App;
+
